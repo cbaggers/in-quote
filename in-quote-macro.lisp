91a3098d1b0efefb39b65f28a50f5f15c856f7a6
Chris Bagley <chris.bagley@gmail.com>
2013-01-17 14:25:34 +0000

    first commit..NOW BACK TO WORK!

---
+++ in-quote-macro.lisp
@@ -0,0 +1,25 @@
+(defmacro ¬ (&rest quoted-data)
+  `(quote ,quoted-data))
+
+(defmacro ¬ (&rest quoted-data)
+  (labels ((process-forms (form quote)
+	     (cond ((null form) nil)
+		   ((atom form) (if quote 
+				    `(quote ,form)
+				    form))
+		   ((and (listp form) (eq '¬ (first form)))
+		    (append (when (not quote) '(list))
+			     (loop :for f :in (rest form)
+				   :collect (process-forms
+					     f (not quote)))))
+		   ((listp form) 
+		    (append (when quote '(list))
+			    (loop :for f :in form
+				  :collect (process-forms
+					    f quote))))
+		   (t (error "how?")))))
+    (process-forms quoted-data t)))
+
+(¬ 1 2 3)
+
+(¬ 1 (2 4) 3 (¬ + 2 3))
